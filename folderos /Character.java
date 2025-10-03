6925205202a3475ca763a669ade18d6c782f8940
Colgate <wojteckib59@gmail.com>
2025-10-03 09:54:27 +0200

    Update Character.java

--- folderos /Character.java
+++ folderos /Character.java
@@ -1,1 +1,11 @@
+public class Character {
+    private String name;
+    private int level;
+    private int health;
 
+    public Character(String name, int level, int health){
+        this.name = name;
+        this.level = level;
+        this.health = health;
+    }
+}
