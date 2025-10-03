62a02f6eda6e620645ed58c18858dcedff0a2709
AndrzejPilarczyk <a00955418@gmail.com>
2025-10-03 10:40:04 +0200

    Update Warrior.java

--- folderos /Warrior.java
+++ folderos /Warrior.java
@@ -1,6 +1,8 @@
 public class Warrior extends Character{
 
-    Warrior(String name, int level, int health){
-        super(name, level, health);
+
+    Warrior(String name, int level, int health, int melee_damage, int magic_damage, int ranged_damage){
+        super(name, level, health, melee_damage, magic_damage, ranged_damage);
     }
 }
+
