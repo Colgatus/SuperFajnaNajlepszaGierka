d285c4ec0c05a21b3ec9eccfc11fcb02018d3928
AndrzejPilarczyk <a00955418@gmail.com>
2025-10-10 10:11:18 +0200

    Update Main.java

--- folderos /Main.java
+++ folderos /Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner newName = new Scanner(System.in);
         String userName;
-        System.out.println("Enter username:");
+        System.out.println("Enter username");
         userName = newName.nextLine();
 
         Scanner newCharacter = new Scanner(System.in);
@@ -11,14 +11,11 @@ public class Main {
         System.out.println("Pick a class: melee, magic, ranged");
         charClass = newCharacter.nextLine();
 
-
-        //Can't get the switch to work
-        
-        //switch(charClass){
-        //   case melee:
-        //        System.out.println("Warrior class chosen");
-        //        break;
-        //}
+        switch(charClass){
+            case "melee":
+                System.out.println(Warrior); //Doesn't work, can't get it to print out the values from object
+                break;
+        }
 
         Warrior warrior = new Warrior(userName, 1, 175, 50, 0, 0);
 
