c5f25658c67392a02353478ea1af898a5ad3534e
Colgate <wojteckib59@gmail.com>
2025-10-16 09:26:24 +0200

    Update Character.java

--- folderos /Character.java
+++ folderos /Character.java
@@ -1,17 +1,26 @@
-public class Character {
-    public String name;
-    public int level;
-    public int health;
-    int melee_damage;
-    int magic_damage;
-    int ranged_damage;
+public abstract class Character {
+    protected String name;
+    protected int level;
+    protected int health;
+    protected int melee_damage;
+    protected int magic_damage;
+    protected int ranged_damage;
 
     public Character(String name, int level, int health, int melee_damage, int magic_damage, int ranged_damage){
         this.name = name;
         this.level = level;
         this.health = health;
         this.melee_damage = melee_damage;
-        this.magic_damage = magic_damage;
+        this.magic_damage = melee_damage;
         this.ranged_damage = ranged_damage;
+    }
+
+    public void showStats() {
+        System.out.println("=== " + name + " ===");
+        System.out.println("Level: " + level);
+        System.out.println("Health: " + health);
+        System.out.println("Melee: " + melee_damage);
+        System.out.println("Magic: " + magic_damage);
+        System.out.println("Ranged: " + ranged_damage);
     }
 }
