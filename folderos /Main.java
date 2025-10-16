6c9f36ddcfa4183e283c72c04cdb3515124c65fe
Colgate <wojteckib59@gmail.com>
2025-10-16 09:25:51 +0200

    Implement main game loop with character creation
    Added a main method for game operations including character creation and combat modes.

--- folderos /Main.java
+++ folderos /Main.java
@@ -1,9 +1,69 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Warrior warrior = new Warrior("John", 1, 175, 50, 0, 0);
 
+void main() {
+    int running = 1;
+    Scanner sc = new Scanner(System.in);
+    List<Character> characters = new ArrayList<>();
 
+    while (running > 0){
 
+        System.out.println("Wybierz operację: 1: Kreator postaci || 2: Walka || 3: wyjdź z gry");
+        int operacja = sc.nextInt();
+
+        switch (operacja){
+            case 1:
+                System.out.println("Wybierz klasę: 1: Warrior || 2: Archer || 3: Mage");
+                int klasa = sc.nextInt();
+
+                System.out.println("Nazwij swoją postać: ");
+                String nazwa = sc.next();
+
+                switch (klasa){
+                    case 1:
+                        System.out.println("Utworzono nową postać - " + nazwa);
+                        characters.add(new Warrior(nazwa));
+                        break;
+                    case 2:
+                        System.out.println("Utworzono nową postać - " + nazwa);
+                        characters.add(new Archer(nazwa));
+                        break;
+                    case 3:
+                        System.out.println("Utworzono nową postać - " + nazwa);
+                        characters.add(new Mage(nazwa));
+                        break;
+                }
+
+                break;
+
+            case 2:
+                System.out.println("Wybierz tryb gry: 1: PvP || 2: PvE");
+                int trybGry = sc.nextInt();
+
+                switch (trybGry){
+                    case 1:
+                        for (Character c : characters){
+                            c.showStats();
+                            System.out.println();
+                        }
+
+                        break;
+
+                    case 2:
+
+                        break;
+                }
+
+                break;
+
+            case 3:
+                System.out.println("Dziękujemy za grę");
+                running = 0;
+                break;
+
+            default:
+                System.out.println("error");
+                break;
+        }
     }
 }
+
