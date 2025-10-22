b65bb023af55eb607e16f898ecf94953d35fecc2
AndrzejPilarczyk <a00955418@gmail.com>
2025-10-22 18:28:16 +0200

    Update Warrior.java

--- folderos /Warrior.java
+++ folderos /Warrior.java
@@ -1,15 +1,49 @@
-public class Warrior extends Character{
+import java.util.Scanner;
 
-    Warrior(String name, int level, int health, int melee_damage, int magic_damage, int ranged_damage){
-        super(name, level, health, melee_damage, magic_damage, ranged_damage);
+public class Warrior extends Character {
+
+    public Warrior(String name) {
+        super(name);
+        this.maxHealth = 140;
+        this.health = maxHealth;
+        this.meleeAttack = 25;
+        this.rangedAttack = 10;
+        this.magicAttack = 5;
     }
+
     @Override
-    public String toString(){
-        return "Name: " + name +
-                " | Level: " + level +
-                " | Health: " + health +
-                " | Melee damage: " + melee_damage +
-                " | Doesn't use magic" +
-                " | Doesn't use ranged";
+    public void attack(Character target) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n=== " + name + " (Warrior) ===");
+        System.out.println("Wybierz typ ataku: 1 - Melee | 2 - Ranged | 3 - Magic");
+        int choice = sc.nextInt();
+
+        int damage = 0;
+        String attackType = "";
+
+        switch (choice) {
+            case 1:
+                damage = getRandomDamage(meleeAttack);
+                attackType = "Melee";
+                break;
+            case 2:
+                damage = getRandomDamage(rangedAttack);
+                attackType = "Ranged";
+                break;
+            case 3:
+                damage = getRandomDamage(magicAttack);
+                attackType = "Magic";
+                break;
+            default:
+                damage = getRandomDamage(meleeAttack);
+                attackType = "Melee";
+                break;
+        }
+
+        System.out.println(name + " używa " + attackType + " ataku i zadaje " + damage + " obrażeń " + target.name + "!");
+        target.health -= damage;
+        if (target.health < 0) target.health = 0;
+        System.out.println(target.name + " ma teraz " + target.health + " HP.");
     }
 }
+
