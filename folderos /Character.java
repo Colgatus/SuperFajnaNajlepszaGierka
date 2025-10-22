244b4875853a6b8d615452b876ef8be245bb60b7
AndrzejPilarczyk <a00955418@gmail.com>
2025-10-22 18:28:00 +0200

    Update Character.java

--- folderos /Character.java
+++ folderos /Character.java
@@ -1,17 +1,57 @@
-public class Character {
-    public String name;
-    public int level;
-    public int health;
-    public int melee_damage;
-    public int magic_damage;
-    public int ranged_damage;
+import java.util.Random;
 
-    public Character(String name, int level, int health, int melee_damage, int magic_damage, int ranged_damage){
+public abstract class Character {
+    protected String name;
+    protected int level;
+    protected int maxHealth;
+    protected int health;
+    protected int meleeAttack;
+    protected int rangedAttack;
+    protected int magicAttack;
+    protected Random rand = new Random();
+
+    public Character(String name) {
         this.name = name;
-        this.level = level;
-        this.health = health;
-        this.melee_damage = melee_damage;
-        this.magic_damage = magic_damage;
-        this.ranged_damage = ranged_damage;
+        this.level = 1;
+        this.maxHealth = 100;
+        this.health = maxHealth;
     }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    protected int getRandomDamage(int baseDamage) {
+        int min = (int) (baseDamage * 0.75);
+        int max = (int) (baseDamage * 1.25);
+        return rand.nextInt(max - min + 1) + min;
+    }
+
+    public abstract void attack(Character target);
+
+    public void showStats() {
+        System.out.println(name + " | LVL: " + level + " | HP: " + health + "/" + maxHealth +
+                " | Melee: " + meleeAttack +
+                " | Ranged: " + rangedAttack +
+                " | Magic: " + magicAttack);
+    }
+
+    public void levelUp() {
+        this.level++;
+        this.maxHealth += 20;
+        this.health = this.maxHealth;
+        this.meleeAttack += 2;
+        this.rangedAttack += 2;
+        this.magicAttack += 2;
+        System.out.println(name + " awansuje na poziom " + level + "! Statystyki zwiększone.");
+    }
+
+    public void silentLevelUp() {
+        this.level++;
+        this.health += 20;
+        this.meleeAttack += 2;
+        this.rangedAttack += 2;
+        this.magicAttack += 2;
+    }
+
 }
