8bb0ae2177f71c443e962bef32a161bf54c7df92
Colgate <wojteckib59@gmail.com>
2025-10-23 03:10:12 +0200

    Implement Slime class extending Character

--- folderos /Slime.java
+++ folderos /Slime.java
@@ -1,1 +1,10 @@
-
+public class Slime extends Character {
+    public Slime(String name) {
+        super(name);
+        this.meleeDamage = 5;
+        this.magicDamage = 0;
+        this.rangedDamage = 0;
+        this.health = 50;
+        this.maxHealth = 50;
+    }
+}
