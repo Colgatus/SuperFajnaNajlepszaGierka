a31afeacceb4a0861320ee288bd1c49919f9ec22
Colgate <wojteckib59@gmail.com>
2025-10-23 03:08:36 +0200

    Refactor Character class to implement Attackable interface

--- folderos /Character.java
+++ folderos /Character.java
@@ -1,26 +1,62 @@
-public abstract class Character {
+public abstract class Character implements Attackable {
     protected String name;
     protected int level;
+    protected int maxHealth;
     protected int health;
-    protected int melee_damage;
-    protected int magic_damage;
-    protected int ranged_damage;
+    protected int meleeDamage;
+    protected int magicDamage;
+    protected int rangedDamage;
 
-    public Character(String name, int level, int health, int melee_damage, int magic_damage, int ranged_damage){
+    public Character(String name) {
         this.name = name;
-        this.level = level;
-        this.health = health;
-        this.melee_damage = melee_damage;
-        this.magic_damage = melee_damage;
-        this.ranged_damage = ranged_damage;
+        this.level = 1;
+        this.maxHealth = 100;
+        this.health = maxHealth;
+        this.meleeDamage = 10;
+        this.magicDamage = 10;
+        this.rangedDamage = 10;
     }
 
     public void showStats() {
-        System.out.println("=== " + name + " ===");
-        System.out.println("Level: " + level);
-        System.out.println("Health: " + health);
-        System.out.println("Melee: " + melee_damage);
-        System.out.println("Magic: " + magic_damage);
-        System.out.println("Ranged: " + ranged_damage);
+        System.out.println(name + " || LVL: " + level + " || HP: " + health + "/" + maxHealth +
+                " || Melee: " + meleeDamage +
+                " || Magic: " + magicDamage +
+                " || Ranged: " + rangedDamage);
+    }
+
+    public void levelUp() {
+        level++;
+        maxHealth += 20;
+        health = maxHealth;
+        meleeDamage += 5;
+        magicDamage += 5;
+        rangedDamage += 5;
+        System.out.println(name + " awansował na poziom " + level + "!");
+        showStats();
+    }
+
+    @Override
+    public void attack(Character target, int attackType) {
+        int damage;
+        String attackName;
+        switch (attackType) {
+            case 1:
+                damage = meleeDamage;
+                attackName = "uderza wręcz";
+                break;
+            case 2:
+                damage = magicDamage;
+                attackName = "uderza zaklęciem";
+                break;
+            case 3:
+                damage = rangedDamage;
+                attackName = "uderza strzałem";
+                break;
+            default:
+                System.out.println("Nieznany typ ataku!");
+                return;
+        }
+        System.out.println(name + " atakuje " + target.name + " " + attackName + " za " + damage + " obrażeń");
+        target.health -= damage;
     }
 }
